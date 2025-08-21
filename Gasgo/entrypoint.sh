f0f50962b1ac53d5e2dbb7dd2cce46a5a3ad0631
Emmanuel Cheruiyot Ruto <117292094+cheruiyotruto@users.noreply.github.com>
2025-08-21 21:53:01 +0300

    vercel deploy

---
+++ Gasgo/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+# Change "portfolio" to your Django project folder name (where wsgi.py lives)
+gunicorn portfolio.wsgi:application --bind 0.0.0.0:8000
