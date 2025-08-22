ca7c2368d26ed879217bd0b74c49f0ffd5520566
Emmanuel Cheruiyot Ruto <117292094+cheruiyotruto@users.noreply.github.com>
2025-08-22 12:16:48 +0300

    fix: make entrypoint executable with debug logs

--- Gasgo/entrypoint.sh
+++ Gasgo/entrypoint.sh
@@ -5,4 +5,4 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 # Change "portfolio" to your Django project folder name (where wsgi.py lives)
-gunicorn portfolio.wsgi:application --bind 0.0.0.0:8000
+gunicorn Gasgo.wsgi:application --bind 0.0.0.0:8000
